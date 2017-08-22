6c3227e08ba9ebddbbf9a62a9acfbb294cf157d1
Ricardo Cerqueira <github@cerqueira.org>
2017-08-22 23:06:24 +0200

    Preliminary support for product builds
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- core/product_config.mk
+++ core/product_config.mk
@@ -173,13 +173,18 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-# Read in all of the product definitions specified by the AndroidProducts.mk
-# files in the tree.
-all_product_configs := $(get-all-product-makefiles)
+ ifneq ($(XOS_BUILD),)
+    $(call import-products, device/xos/$(XOS_BUILD)/XOS.mk)
+    all_product_configs := device/xos/$(XOS_BUILD)/XOS.mk
+  else
+    # Read in all of the product definitions specified by the AndroidProducts.mk
+    # files in the tree.
+    all_product_configs := $(get-all-product-makefiles)
+  endif # XOS_BUILD
 endif
 
 all_named_products :=
-
+ifeq ($(XOS_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
@@ -203,6 +208,10 @@ $(foreach f, $(all_product_configs),\
 _cpm_words :=
 _cpm_word1 :=
 _cpm_word2 :=
+else
+    current_product_makefile := $(strip $(all_product_configs))
+    all_product_makefiles := $(strip $(all_product_configs))
+endif
 current_product_makefile := $(strip $(current_product_makefile))
 all_product_makefiles := $(strip $(all_product_makefiles))
 
