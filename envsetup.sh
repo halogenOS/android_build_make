2c9fafbb2e8d85a2f13548341340f4294b87c381
Chirayu Desai <chirayudesai1@gmail.com>
2017-09-17 17:06:45 -0400

    Fix lunch while using roomservice
    Change-Id: I9dea4605857741132b8d10143c20d4bd7829086a
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- envsetup.sh
+++ envsetup.sh
@@ -660,10 +660,6 @@ function lunch()
     fi
 
     check_product $product
-    TARGET_PRODUCT=$product \
-    TARGET_BUILD_VARIANT=$variant \
-    TARGET_PLATFORM_VERSION=$version \
-    build_build_var_cache
     if [ $? -ne 0 ]
     then
         # if we can't find the product, try to grab it from our github
@@ -678,6 +674,10 @@ function lunch()
         build/make/tools/roomservice.py $product true
         popd > /dev/null
     fi
+    TARGET_PRODUCT=$product \
+    TARGET_BUILD_VARIANT=$variant \
+    TARGET_PLATFORM_VERSION=$version \
+    build_build_var_cache
     if [ $? -ne 0 ]
     then
         echo
