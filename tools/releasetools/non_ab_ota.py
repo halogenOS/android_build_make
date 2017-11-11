fe2d217d7105f73e99864a76ae0abfcaeae29dce
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-11 17:24:03 -0200

    Show ROM info while flashing
    [windowz414: Make it more global by using CUSTOM_VENDOR_DIR]
    
    Change-Id: I0769a74d70621536d3794bcdc3e70847945f00b5
    Signed-off-by: Beru Hinode <beru@halogenos.org>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- tools/releasetools/non_ab_ota.py
+++ tools/releasetools/non_ab_ota.py
@@ -222,6 +222,15 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("Target: {}".format(target_info.fingerprint))
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
+
+  android_version = target_info.GetBuildProp("ro.build.version.release")
+  build_id = target_info.GetBuildProp("ro.build.id")
+  build_date = target_info.GetBuildProp("ro.build.date")
+  security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
+  device = target_info.GetBuildProp("ro.custom.device")
+  script.PrintCustomBanner(android_version, build_id, build_date,
+                                  security_patch, device)
+
   device_specific.FullOTA_InstallBegin()
 
   CopyInstallTools(output_zip)
