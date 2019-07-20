4cd13d61ad15493cac111ef45c3e3f93fce40e5c
Simao Gomes Viana <xdevs23@outlook.com>
2019-07-20 23:01:29 +0200

    releasetools: xosify updater-script
    Change-Id: I076ef6c56321c6cb1823c062a754469125d72b3b

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -835,8 +835,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   device = target_info.GetBuildProp("org.pixelexperience.device")
 
   script.Print("----------------------------------------------");
-  script.Print("              Pixel Experience");
-  script.Print("               by jhenrique09");
+  script.Print("                  halogenOS");
   script.Print("----------------------------------------------");
   script.Print(" Android version: %s"%(android_version));
   script.Print(" Build id: %s"%(build_id));
