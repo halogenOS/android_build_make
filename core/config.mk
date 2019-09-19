b5d0482f9587833a678ee5b12d3d48a54706db54
Simao Gomes Viana <xdevs23@outlook.com>
2019-09-19 22:36:09 +0200

    core: config: correct BoardConfigCustom include
    Change-Id: I15e84e77f47069c4af9f1dae386b424753546cea

--- core/config.mk
+++ core/config.mk
@@ -234,7 +234,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 ifneq ($(CUSTOM_BUILD),)
-include vendor/aosp/config/BoardConfig.mk
+include $(CUSTOM_VENDOR_DIR)/config/BoardConfigCustom.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
