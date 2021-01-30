be504889b98797e0942292ac661999e898345eeb
Chirayu Desai <chirayudesai1@gmail.com>
2021-01-30 19:19:01 +0530

    Replace old-style kernel headers only when building inline
    Change-Id: I71b891928a13771c176adc0eaa79b46503cb176f

--- core/binary.mk
+++ core/binary.mk
@@ -64,6 +64,7 @@ endif
 ifneq (,$(findstring $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr,$(LOCAL_ADDITIONAL_DEPENDENCIES)))
   LOCAL_ADDITIONAL_DEPENDENCIES := $(patsubst $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr,,$(LOCAL_ADDITIONAL_DEPENDENCIES))
 endif
+endif
 
 # Replace device_kernel_headers with generated_kernel_headers
 ifneq (,$(findstring device_kernel_headers,$(LOCAL_HEADER_LIBRARIES)))
