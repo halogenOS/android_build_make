halogenOS/android_build_make

commit 6cbdaf9838b16798b77a3d7dd6f791d5ac5970c4

de-potato-ify
Change-Id: Ia5cb80d3ded086e591b6d8a4a274b8c0d39c82a0

core/config.mk
@@ -317,8 +317,8 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
-ifneq ($(POTATO_BUILD),)
-include vendor/potato/config/BoardConfigPotato.mk
+ifneq ($(CUSTOM_BUILD),)
+include $CUSTOM_VENDOR_DIR/config/BoardConfigCustom.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
@@ -1164,11 +1164,11 @@ dont_bother_goals := out \
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
-ifneq ($(POTATO_BUILD),)
-ifneq ($(wildcard device/potato/sepolicy/common/sepolicy.mk),)
+ifneq ($(CUSTOM_BUILD),)
+ifneq ($(wildcard device/custom/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/potato/sepolicy/common/sepolicy.mk)
+$(eval include device/custom/sepolicy/common/sepolicy.mk)
 endif
 endif
 

core/Makefile
@@ -5430,7 +5430,7 @@ $(INTERNAL_OTA_PACKAGE_TARGET): $(BUILT_TARGET_FILES_PACKAGE) $(OTA_FROM_TARGET_
 .PHONY: otapackage
 otapackage: $(INTERNAL_OTA_PACKAGE_TARGET)
 	echo ""
-	cat $(gettop)/build/make/potato_ascii_logo
+	cat $(gettop)/build/make/custom_ascii_logo
 	echo ""
 
 ifeq ($(BOARD_BUILD_RETROFIT_DYNAMIC_PARTITIONS_OTA_PACKAGE),true)

core/main.mk
@@ -469,7 +469,7 @@ ADDITIONAL_SYSTEM_PROPERTIES += net.bt.name=Android
 
 # ------------------------------------------------------------
 # Include vendor specific additions to build properties
--include vendor/potato/build/core/main.mk
+-include $CUSTOM_VENDOR_DIR/build/core/main.mk
 
 # ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns

core/pathmap.mk
@@ -93,4 +93,4 @@ FRAMEWORKS_BASE_SUBDIRS := \
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
 
--include vendor/potato/build/core/pathmap.mk
+-include $CUSTOM_VENDOR_DIR/build/core/pathmap.mk

core/sysprop.mk
@@ -251,7 +251,7 @@ $(gen_from_buildinfo_sh): $(INTERNAL_BUILD_ID_MAKEFILE) $(API_FINGERPRINT) | $(B
 	$(hide) TARGET_BUILD_TYPE="$(TARGET_BUILD_VARIANT)" \
 	        TARGET_BUILD_FLAVOR="$(TARGET_BUILD_FLAVOR)" \
 	        TARGET_DEVICE="$(TARGET_DEVICE)" \
-	        POTATO_DEVICE="$(TARGET_DEVICE)" \
+	        CUSTOM_DEVICE="$(TARGET_DEVICE)" \
 	        PRODUCT_DEFAULT_LOCALE="$(call get-default-product-locale,$(PRODUCT_LOCALES))" \
 	        PRODUCT_DEFAULT_WIFI_CHANNELS="$(PRODUCT_DEFAULT_WIFI_CHANNELS)" \
 	        PRIVATE_BUILD_DESC="$(BUILD_DESC)" \

custom_ascii_logo
@@ -1,0 +1,6 @@
+ _           _                         ___  ____
+| |__   __ _| | ___   __ _  ___ _ __  / _ \/ ___|
+| '_ \ / _` | |/ _ \ / _` |/ _ \ '_ \| | | \___ \
+| | | | (_| | | (_) | (_| |  __/ | | | |_| |___) |
+|_| |_|\__,_|_|\___/ \__, |\___|_| |_|\___/|____/
+                     |___/

envsetup.sh
@@ -46,7 +46,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 
 EOF
 
-    __print_potato_functions_help
+    __print_custom_functions_help
 
 cat <<EOF
 
@@ -59,7 +59,7 @@ EOF
     local T=$(gettop)
     local A=""
     local i
-    for i in `cat $T/build/envsetup.sh $T/vendor/potato/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
+    for i in `cat $T/build/envsetup.sh $T/$CUSTOM_VENDOR_DIR/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
       A="$A $i"
     done
     echo $A
@@ -70,8 +70,8 @@ function build_build_var_cache()
 {
     local T=$(gettop)
     # Grep out the variable names from the script.
-    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/potato/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
-    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/potato/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_vars=(`cat $T/build/envsetup.sh $T/$CUSTOM_VENDOR_DIR/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_abs_vars=(`cat $T/build/envsetup.sh $T/$CUSTOM_VENDOR_DIR/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
     # Call the build system to dump the "<val>=<value>" pairs as a shell script.
     build_dicts_script=`\builtin cd $T; build/soong/soong_ui.bash --dumpvars-mode \
                         --vars="${cached_vars[*]}" \
@@ -153,12 +153,12 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-    if (echo -n $1 | grep -q -e "^potato_") ; then
-        POTATO_BUILD=$(echo -n $1 | sed -e 's/^potato_//g')
+    if (echo -n $1 | grep -q -e "^custom_") ; then
+        CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^custom_//g')
     else
-        POTATO_BUILD=
+        CUSTOM_BUILD=
     fi
-    export POTATO_BUILD
+    export CUSTOM_BUILD
 
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
@@ -714,13 +714,13 @@ function lunch()
         # if we can't find a product, try to grab it from our github
         T=$(gettop)
         cd $T > /dev/null
-        vendor/potato/build/tools/roomservice.py $product
+        $CUSTOM_VENDOR_DIR/build/tools/roomservice.py $product
         cd - > /dev/null
         check_product $product
     else
         T=$(gettop)
         cd $T > /dev/null
-        vendor/potato/build/tools/roomservice.py $product true
+        $CUSTOM_VENDOR_DIR/build/tools/roomservice.py $product true
         cd - > /dev/null
     fi
 
@@ -1899,4 +1899,4 @@ fi
 
 export ANDROID_BUILD_TOP=$(gettop)
 
-. $ANDROID_BUILD_TOP/vendor/potato/build/envsetup.sh
+. $ANDROID_BUILD_TOP/$CUSTOM_VENDOR_DIR/build/envsetup.sh

potato_ascii_logo
@@ -1,30 +1,0 @@
-  [38;5;203m [39m[38;5;203m [39m[38;5;203m [39m[38;5;203m [39m[38;5;203m [39m[38;5;203m [39m[38;5;203m [39m[38;5;203m [39m[38;5;198m [39m[38;5;198m [39m[38;5;198m [39m[38;5;198m [39m[38;5;198m [39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m [39m[38;5;163m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;128m[39m
-  [38;5;203m [39m[38;5;203m [39m[38;5;203m [39m[38;5;203m [39m[38;5;203m [39m[38;5;198m [39m[38;5;198m [39m[38;5;198m [39m[38;5;198m [39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;163m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;128m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m[39m
-  [38;5;203m [39m[38;5;203m [39m[38;5;198m [39m[38;5;198m [39m[38;5;198m [39m[38;5;198m [39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;163m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;128m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m[39m
-  [38;5;198m [39m[38;5;198m [39m[38;5;198m [39m[38;5;198m [39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;199m@[39m[38;5;199m [39m[38;5;199m [39m[38;5;199m [39m[38;5;199m [39m[38;5;199m [39m[38;5;199m [39m[38;5;199m@[39m[38;5;199m@[39m[38;5;163m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;128m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m[39m
-  [38;5;198m [39m[38;5;198m [39m[38;5;198m [39m[38;5;198m@[39m[38;5;198m@[39m[38;5;198m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m [39m[38;5;199m [39m[38;5;199m [39m[38;5;199m [39m[38;5;199m [39m[38;5;199m [39m[38;5;199m [39m[38;5;163m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;128m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m[39m
-  [38;5;198m [39m[38;5;198m@[39m[38;5;198m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m [39m[38;5;199m [39m[38;5;199m [39m[38;5;199m [39m[38;5;199m [39m[38;5;163m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m#[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;128m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m [39m[38;5;93m [39m[38;5;93m[39m
-  [38;5;199m [39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m [39m[38;5;199m [39m[38;5;163m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;128m#[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m [39m[38;5;93m[39m
-  [38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;163m@[39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;128m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;63m@[39m[38;5;63m[39m
-  [38;5;199m@[39m[38;5;199m@[39m[38;5;199m@[39m[38;5;163m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;128m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m[39m
-  [38;5;163m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;164m [39m[38;5;128m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m[39m
-  [38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;128m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m[39m
-  [38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;128m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m([39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;33m@[39m[38;5;33m[39m
-  [38;5;164m@[39m[38;5;164m@[39m[38;5;164m@[39m[38;5;128m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m&[39m[38;5;129m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m[39m
-  [38;5;128m [39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m [39m[38;5;33m[39m
-  [38;5;129m [39m[38;5;129m [39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;129m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m&[39m[38;5;93m [39m[38;5;93m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;33m [39m[38;5;33m [39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m [39m[38;5;33m [39m[38;5;39m[39m
-  [38;5;129m [39m[38;5;129m [39m[38;5;129m [39m[38;5;129m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;33m [39m[38;5;33m [39m[38;5;33m [39m[38;5;33m [39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;39m[39m
-  [38;5;129m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;93m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;33m [39m[38;5;33m [39m[38;5;33m [39m[38;5;33m [39m[38;5;33m%[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;39m@[39m[38;5;39m@[39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;39m[39m
-  [38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m@[39m[38;5;93m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;39m@[39m[38;5;39m@[39m[38;5;39m@[39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;38m[39m
-  [38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;93m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;63m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;39m@[39m[38;5;39m@[39m[38;5;39m@[39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;38m[39m
-  [38;5;93m [39m[38;5;93m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m [39m[38;5;63m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;33m@[39m[38;5;39m@[39m[38;5;39m@[39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;39m [39m[38;5;38m [39m[38;5;44m [39m[38;5;44m [39m[38;5;44m [39m[38;5;44m [39m[38;5;44m [39m[38;5;44m[39m
-  
-  [38;5;163m [0m[38;5;163m/[0m[38;5;164m#[0m[38;5;164m#[0m[38;5;164m#[0m[38;5;164m#[0m[38;5;164m#[0m[38;5;164m#[0m[38;5;164m#[0m[38;5;164m [0m[38;5;164m [0m[38;5;164m [0m[38;5;128m/[0m[38;5;128m#[0m[38;5;129m#[0m[38;5;129m#[0m[38;5;129m#[0m[38;5;129m#[0m[38;5;129m#[0m[38;5;129m [0m[38;5;129m [0m[38;5;129m [0m[38;5;129m/[0m[38;5;93m#[0m[38;5;93m#[0m[38;5;93m#[0m[38;5;93m#[0m[38;5;93m#[0m[38;5;93m#[0m[38;5;93m [0m[38;5;93m [0m[38;5;93m/[0m[38;5;99m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m [0m
-  [38;5;164m|[0m[38;5;164m [0m[38;5;164m#[0m[38;5;164m#[0m[38;5;164m_[0m[38;5;164m_[0m[38;5;164m [0m[38;5;164m [0m[38;5;164m#[0m[38;5;128m#[0m[38;5;128m [0m[38;5;129m/[0m[38;5;129m#[0m[38;5;129m#[0m[38;5;129m_[0m[38;5;129m_[0m[38;5;129m [0m[38;5;129m [0m[38;5;129m#[0m[38;5;129m#[0m[38;5;93m [0m[38;5;93m/[0m[38;5;93m#[0m[38;5;93m#[0m[38;5;93m_[0m[38;5;93m_[0m[38;5;93m [0m[38;5;93m [0m[38;5;93m#[0m[38;5;99m#[0m[38;5;63m|[0m[38;5;63m [0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m_[0m[38;5;63m_[0m[38;5;63m [0m[38;5;63m [0m[38;5;63m#[0m[38;5;63m#[0m
-  [38;5;164m|[0m[38;5;164m [0m[38;5;164m#[0m[38;5;164m#[0m[38;5;164m [0m[38;5;164m [0m[38;5;128m\[0m[38;5;128m [0m[38;5;129m#[0m[38;5;129m#[0m[38;5;129m|[0m[38;5;129m [0m[38;5;129m#[0m[38;5;129m#[0m[38;5;129m [0m[38;5;129m [0m[38;5;129m\[0m[38;5;93m [0m[38;5;93m#[0m[38;5;93m#[0m[38;5;93m|[0m[38;5;93m [0m[38;5;93m#[0m[38;5;93m#[0m[38;5;93m [0m[38;5;93m [0m[38;5;99m\[0m[38;5;63m_[0m[38;5;63m_[0m[38;5;63m/[0m[38;5;63m|[0m[38;5;63m [0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m [0m[38;5;63m [0m[38;5;63m\[0m[38;5;63m [0m[38;5;69m#[0m[38;5;33m#[0m
-  [38;5;164m|[0m[38;5;164m [0m[38;5;164m#[0m[38;5;128m#[0m[38;5;128m#[0m[38;5;129m#[0m[38;5;129m#[0m[38;5;129m#[0m[38;5;129m#[0m[38;5;129m/[0m[38;5;129m|[0m[38;5;129m [0m[38;5;129m#[0m[38;5;129m#[0m[38;5;93m [0m[38;5;93m [0m[38;5;93m|[0m[38;5;93m [0m[38;5;93m#[0m[38;5;93m#[0m[38;5;93m|[0m[38;5;93m [0m[38;5;93m [0m[38;5;99m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m [0m[38;5;63m|[0m[38;5;63m [0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;69m#[0m[38;5;33m#[0m[38;5;33m#[0m[38;5;33m#[0m[38;5;33m/[0m
-  [38;5;128m|[0m[38;5;128m [0m[38;5;129m#[0m[38;5;129m#[0m[38;5;129m_[0m[38;5;129m_[0m[38;5;129m_[0m[38;5;129m_[0m[38;5;129m/[0m[38;5;129m [0m[38;5;129m|[0m[38;5;93m [0m[38;5;93m#[0m[38;5;93m#[0m[38;5;93m [0m[38;5;93m [0m[38;5;93m|[0m[38;5;93m [0m[38;5;93m#[0m[38;5;93m#[0m[38;5;99m [0m[38;5;63m\[0m[38;5;63m_[0m[38;5;63m_[0m[38;5;63m_[0m[38;5;63m_[0m[38;5;63m [0m[38;5;63m [0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m|[0m[38;5;63m [0m[38;5;69m#[0m[38;5;33m#[0m[38;5;33m_[0m[38;5;33m_[0m[38;5;33m_[0m[38;5;33m_[0m[38;5;33m/[0m[38;5;33m [0m
-  [38;5;129m|[0m[38;5;129m [0m[38;5;129m#[0m[38;5;129m#[0m[38;5;129m [0m[38;5;129m [0m[38;5;129m [0m[38;5;129m [0m[38;5;93m [0m[38;5;93m [0m[38;5;93m|[0m[38;5;93m [0m[38;5;93m#[0m[38;5;93m#[0m[38;5;93m [0m[38;5;93m [0m[38;5;93m|[0m[38;5;99m [0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m [0m[38;5;63m/[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m [0m[38;5;63m [0m[38;5;63m\[0m[38;5;63m [0m[38;5;63m#[0m[38;5;69m#[0m[38;5;33m|[0m[38;5;33m [0m[38;5;33m#[0m[38;5;33m#[0m[38;5;33m [0m[38;5;33m [0m[38;5;33m [0m[38;5;33m [0m[38;5;33m [0m[38;5;39m [0m
-  [38;5;129m|[0m[38;5;129m [0m[38;5;129m#[0m[38;5;129m#[0m[38;5;129m [0m[38;5;93m [0m[38;5;93m [0m[38;5;93m [0m[38;5;93m [0m[38;5;93m [0m[38;5;93m|[0m[38;5;93m [0m[38;5;93m [0m[38;5;93m#[0m[38;5;99m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m/[0m[38;5;63m|[0m[38;5;63m [0m[38;5;63m [0m[38;5;63m#[0m[38;5;63m#[0m[38;5;63m#[0m[38;5;69m#[0m[38;5;33m#[0m[38;5;33m#[0m[38;5;33m/[0m[38;5;33m|[0m[38;5;33m [0m[38;5;33m#[0m[38;5;33m#[0m[38;5;33m [0m[38;5;33m [0m[38;5;39m [0m[38;5;39m [0m[38;5;39m [0m[38;5;39m [0m
-  [38;5;129m|[0m[38;5;129m_[0m[38;5;93m_[0m[38;5;93m/[0m[38;5;93m [0m[38;5;93m [0m[38;5;93m [0m[38;5;93m [0m[38;5;93m [0m[38;5;93m [0m[38;5;93m [0m[38;5;99m\[0m[38;5;63m_[0m[38;5;63m_[0m[38;5;63m_[0m[38;5;63m_[0m[38;5;63m_[0m[38;5;63m_[0m[38;5;63m/[0m[38;5;63m [0m[38;5;63m [0m[38;5;63m\[0m[38;5;63m_[0m[38;5;69m_[0m[38;5;33m_[0m[38;5;33m_[0m[38;5;33m_[0m[38;5;33m_[0m[38;5;33m/[0m[38;5;33m [0m[38;5;33m|[0m[38;5;33m_[0m[38;5;33m_[0m[38;5;39m/[0m[38;5;39m [0m[38;5;39m [0m[38;5;39m [0m[38;5;39m [0m[38;5;39m [0m
-  [38;5;93m [0m

commit 02f9f0a5b57bb9b46eb1e5d45c33de230ec6b028

core/config: Fix use of `CUSTOM_VENDOR_DIR`
* Make utilizes variables as `$(variable)`, not `$variable`.

Fixes: 6cbdaf9838b16798b77a3d7dd6f791d5ac5970c4
Signed-off-by: Beru Shinsetsu <windowz414@1337.lgbt>

core/config.mk
@@ -318,7 +318,7 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
 ifneq ($(CUSTOM_BUILD),)
-include $CUSTOM_VENDOR_DIR/config/BoardConfigCustom.mk
+include $(CUSTOM_VENDOR_DIR)/config/BoardConfigCustom.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
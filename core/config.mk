02f9f0a5b57bb9b46eb1e5d45c33de230ec6b028
Beru Shinsetsu <windowz414@1337.lgbt>
2022-02-22 07:22:27 +0000

    core/config: Fix use of `CUSTOM_VENDOR_DIR`
    * Make utilizes variables as `$(variable)`, not `$variable`.
    
    Fixes: 6cbdaf9838b16798b77a3d7dd6f791d5ac5970c4
    Signed-off-by: Beru Shinsetsu <windowz414@1337.lgbt>

--- core/config.mk
+++ core/config.mk
@@ -318,7 +318,7 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
 ifneq ($(CUSTOM_BUILD),)
-include $CUSTOM_VENDOR_DIR/config/BoardConfigCustom.mk
+include $(CUSTOM_VENDOR_DIR)/config/BoardConfigCustom.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
