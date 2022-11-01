1c5de6713ba62398a0dec2a00952102d947edfee
Simão Gomes Viana <devel@superboring.dev>
2022-11-01 16:47:48 +0100

    Include product makefiles
    Reference: https://github.com/LineageOS/android_build
    
    Change-Id: I33f9f417477aacddd04c2a4671721c937d5270a7
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -92,3 +92,5 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+include $(CUSTOM_PRODUCT_DIR)/build/core/pathmap.mk
