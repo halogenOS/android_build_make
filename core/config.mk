e16e4ff96066d80861d144822b244abf039fe4e0
Simão Gomes Viana <devel@superboring.dev>
2022-11-14 00:24:52 +0100

    Include product config

--- core/config.mk
+++ core/config.mk
@@ -1274,4 +1274,6 @@ endif
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
+include $(CUSTOM_PRODUCT_DIR)/build/core/config.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
