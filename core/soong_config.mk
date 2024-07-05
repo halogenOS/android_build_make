01e9ab2a1768ec47ee773405ac52d98837b38807
Simão Gomes Viana <devel@superboring.dev>
2024-07-05 22:57:34 +0200

    Add support for overriding security patch
    In the case that we pick the patches from the bulletin, we only change
    the patch level to *-01 and not *-05 since the *-05 bulletins only
    contain vendor-specific patches that are either proprietary or in the
    kernel. That is because we don't have the ability to control these patches
    since maintainers need to rely on the oem, odm and SoC vendor to update
    these components.
    
    For maintainers that are able to get free (libre) components up to date,
    this patch adds support for overriding the security patch level.
    
    In order to override the security patch level, define the variable
    PLATFORM_SECURITY_PATCH_OVERRIDE in the aosp_*.mk of your device
    tree.
    
    For example:
    
    PLATFORM_SECURITY_PATCH_OVERRIDE := 2024-07-05
    
    At the moment, no date comparison is performed, so make sure you update
    this whenever the original platform security patch changes. A change may
    be added in the future that checks for this and uses the level that is
    more up to date.

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -42,7 +42,11 @@ $(call add_json_val,  Platform_sdk_extension_version,    $(PLATFORM_SDK_EXTENSIO
 $(call add_json_val,  Platform_base_sdk_extension_version, $(PLATFORM_BASE_SDK_EXTENSION_VERSION))
 $(call add_json_csv,  Platform_version_active_codenames, $(PLATFORM_VERSION_ALL_CODENAMES))
 $(call add_json_csv,  Platform_version_all_preview_codenames, $(PLATFORM_VERSION_ALL_PREVIEW_CODENAMES))
-$(call add_json_str,  Platform_security_patch,           $(PLATFORM_SECURITY_PATCH))
+PLATFORM_SECURITY_PATCH_FINAL := $(PLATFORM_SECURITY_PATCH)
+ifdef PLATFORM_SECURITY_PATCH_OVERRIDE
+  PLATFORM_SECURITY_PATCH_FINAL := $(PLATFORM_SECURITY_PATCH_OVERRIDE)
+endif
+$(call add_json_str,  Platform_security_patch,           $(PLATFORM_SECURITY_PATCH_FINAL))
 $(call add_json_str,  Platform_preview_sdk_version,      $(PLATFORM_PREVIEW_SDK_VERSION))
 $(call add_json_str,  Platform_base_os,                  $(PLATFORM_BASE_OS))
 $(call add_json_str,  Platform_version_last_stable,      $(PLATFORM_VERSION_LAST_STABLE))
